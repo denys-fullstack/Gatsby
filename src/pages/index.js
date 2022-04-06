56da35fb1273ae7e77fc99861543c6d2fa2450ef
StarDev0819 <denorzahar@gmail.com>
2022-04-07 02:12:13 +0800

    Use images from local

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        alt="Beautiful girl"
+        src="../images/2.png"
       />
     </Layout>
   )
