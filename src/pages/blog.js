e68ee20b902edeeba0c4710940f6d348ea6bed4d
StarDev0819 <denorzahar@gmail.com>
2022-04-17 13:57:39 +0800

    fix blog error

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
-import Layout from '../../components/layout'
+import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
   return (
@@ -26,8 +26,8 @@ export const query = graphql`
     allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
         frontmatter {
-          date(formatString: "MMMM D, YYYY")
           title
+          date(formatString: "MMMM DD, YYYY")
         }
         id
         body
